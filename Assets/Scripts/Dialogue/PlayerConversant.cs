577b69d05b291477f9e83f75c127c5ca95f2467e
Arhan Busam <arhan.busam@gmail.com>
2021-06-06 15:20:51 +1000

    Lost some files

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55520a0dcc498bcab80fa11d20cbf6ac32ff0fecfc348b47392816836f2f1976
+size 1240
---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f420344cd17ca0da76b341cf31745ceeec22afb03af54a69c6dd7931a9649bf3
+size 1365
