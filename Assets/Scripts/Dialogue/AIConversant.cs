36a1352bc5bd16ccbc0dc8aa5eb7fbe494de0422
Arhan Busam <arhan.busam@gmail.com>
2021-06-06 15:15:57 +1000

    Saved some files

---
+++ Assets/Scripts/Dialogue/AIConversant.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7271bc664041447b0f1530b389084f0c1633169ed605d32f7fa644357413f970
+size 737
--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9465f8a319e7854349eb1e07c9175fd72f39cd7ec28f4f64df130e43a6fe43c
-size 1850
+oid sha256:58fa94d90589b0fa9a6d87c2108076aeda52d6fbaf596f93bb4ae0ff8e2b6504
+size 2365
