7f76d1f029cfbe68a156a6a14bf3ae4d0c0b94cf
Arhan Busam <arhan.busam@gmail.com>
2021-06-14 10:07:00 +1000

    Started on completed quests

--- Assets/Scripts/UI/Quests/QuestListUI.cs
+++ Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -8,6 +8,8 @@ namespace RPG.UI.Quests
     public class QuestListUI : MonoBehaviour
     {
         [SerializeField] QuestItemUI questPrefab;
+        [SerializeField] Transform incompleteQuestList;
+        [SerializeField] Transform completedQuestList;
 
         void Start()
         {
@@ -18,15 +20,24 @@ namespace RPG.UI.Quests
 
         private void UpdateUI()
         {
-            foreach (Transform item in this.transform)
-            {
-                Destroy(item.gameObject);
-            }
+            DestroyItems();
             QuestList questList = GameObject.FindGameObjectWithTag("Player").GetComponent<QuestList>();
             foreach (QuestStatus status in questList.Statuses)
             {
-                QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, this.transform);
+                QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, incompleteQuestList.transform);
                 uiInstance.Setup(status);
+            }
+        }
+
+        private void DestroyItems()
+        {
+            foreach (Transform item in incompleteQuestList.transform)
+            {
+                Destroy(item.gameObject);
+            }
+            foreach (Transform item in completedQuestList.transform)
+            {
+                Destroy(item.gameObject);
             }
         }
     }
