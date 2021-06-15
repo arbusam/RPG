829408f490a39c70ea0e93c3c409c9c0978f935b
Arhan Busam <arhan.busam@gmail.com>
2021-06-15 18:37:03 +1000

    Component APIs

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GameDevTV.Inventories;
@@ -14,11 +15,17 @@ namespace RPG.Shops
             float price;
             int quantityInTransaction;
         }
+        
+        public event Action onChange;
 
         public IEnumerable<ShopItem> GetFilteredItems() { return null; }
         public void SelectFilter(ItemCategory category) {}
         public ItemCategory GetFilter() { return ItemCategory.None; }
         public void SelectMode(bool isBuying) {}
+        public bool IsBuyingMode() { return true; }
+        public bool CanTransact() { return true; }
         public void ConfirmTransaction() {}
+        public float TransactionTotal() { return 0; }
+        public void AddToTransaction(InventoryItem item, int quantity) {}
     }
 }
