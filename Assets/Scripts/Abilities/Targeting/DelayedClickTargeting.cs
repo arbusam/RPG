8ed1a7b89b56129261279f3ea5a9aa9385866004
Arhan Busam <arhan.busam@gmail.com>
2021-06-26 21:00:01 +1000

    Daily Commit Sat Jun 26 21:00:01 AEST 2021

--- Assets/Scripts/Abilities/Targeting/DelayedClickTargeting.cs
+++ Assets/Scripts/Abilities/Targeting/DelayedClickTargeting.cs
@@ -1,13 +1,39 @@
+using System.Collections;
+using RPG.Control;
 using UnityEngine;
 
 namespace RPG.Abilities.Targeting
 {
-    [CreateAssetMenu(fileName = "Demo Targeting", menuName = "RPG/Abilities/Targeting/Demo")]
-    public class DemoTargeting : TargetingStrategy
+    [CreateAssetMenu(fileName = "Delayed Click Targeting", menuName = "RPG/Abilities/Targeting/Delayed Click")]
+    public class DelayedClickTargeting : TargetingStrategy
     {
-        public override void StartTargeting()
+        [SerializeField] Texture2D cursorTexture;
+        [SerializeField] Vector2 cursorHotspot;
+
+        public override void StartTargeting(GameObject user)
         {
-            Debug.Log("Demo Targeting Selected");
+            PlayerControls playerControls = user.GetComponent<PlayerControls>();
+            playerControls.StartCoroutine(Targeting(user, playerControls));
+        }
+
+        private IEnumerator Targeting(GameObject user, PlayerControls playerControls)
+        {
+            playerControls.enabled = false;
+            while (true)
+            {
+                Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    while (Input.GetMouseButtonDown(0))
+                    {
+                        yield return null;
+                    }
+                    playerControls.enabled = true;
+                    yield break;
+                }
+                yield return null;
+            }
         }
     }
 }
--- Assets/Scripts/Abilities/Targeting/DemoTargeting.cs
+++ Assets/Scripts/Abilities/Targeting/DemoTargeting.cs
@@ -5,7 +5,7 @@ namespace RPG.Abilities.Targeting
     [CreateAssetMenu(fileName = "Demo Targeting", menuName = "RPG/Abilities/Targeting/Demo")]
     public class DemoTargeting : TargetingStrategy
     {
-        public override void StartTargeting()
+        public override void StartTargeting(GameObject user)
         {
             Debug.Log("Demo Targeting Selected");
         }
