df411d9f20100fd779217bdf37bcb51a15799d9a
Arhan Busam <arhan.busam@gmail.com>
2021-07-09 10:08:54 +1000

    Other

--- cron/commits.sh
+++ cron/commits.sh
@@ -1,2 +1,2 @@
 # Git: add and commit changes
-cd "/Users/arhanbusam/Dropbox/Arhan's Documents/Unity/RPG" && /usr/bin/git add --all && /usr/bin/git commit -m "Daily Commit `date`" && /usr/bin/git push
+# cd "/Users/arhanbusam/Dropbox/Arhan's Documents/Unity/RPG" && /usr/bin/git add --all && /usr/bin/git commit -m "Daily Commit `date`" && /usr/bin/git push
