bf4f9825ee495bc0e21cbb4a576f3c75034112f4
Arhan Busam <arhan.busam@gmail.com>
2021-07-11 11:21:39 +1000

    Enhancement Wrap Up

--- Assets/Scripts/Stats/EnhancementStore.cs
+++ Assets/Scripts/Stats/EnhancementStore.cs
@@ -1,12 +1,46 @@
 using System.Collections.Generic;
+using GameDevTV.Saving;
 using UnityEngine;
 
 namespace RPG.Stats
 {
-    public class EnhancementStore : MonoBehaviour
+    public class EnhancementStore : MonoBehaviour, IModifierProvider, ISaveable
     {
+        [SerializeField] EnhancementBonus[] bonusConfig;
+
+        [System.Serializable]
+        public class EnhancementBonus
+        {
+            public EnhancementCategory category;
+            public Stat stat;
+            public float additiveBonusPerPoint = 0;
+            public float percentageBonusPerPoint = 0;
+        }
+
         Dictionary<EnhancementCategory, int> assignedPoints = new Dictionary<EnhancementCategory, int>();
         Dictionary<EnhancementCategory, int> stagedPoints = new Dictionary<EnhancementCategory, int>();
+
+        Dictionary<Stat, Dictionary<EnhancementCategory, float>> additiveBonusCache;
+        Dictionary<Stat, Dictionary<EnhancementCategory, float>> percentageBonusCache;
+
+        private void Awake()
+        {
+            additiveBonusCache = new Dictionary<Stat, Dictionary<EnhancementCategory, float>>();
+            percentageBonusCache = new Dictionary<Stat, Dictionary<EnhancementCategory, float>>();
+            foreach (var bonus in bonusConfig)
+            {
+                if (!additiveBonusCache.ContainsKey(bonus.stat))
+                {
+                    additiveBonusCache[bonus.stat] = new Dictionary<EnhancementCategory, float>();
+                }
+                if (!percentageBonusCache.ContainsKey(bonus.stat))
+                {
+                    percentageBonusCache[bonus.stat] = new Dictionary<EnhancementCategory, float>();
+                }
+                additiveBonusCache[bonus.stat][bonus.category] = bonus.additiveBonusPerPoint;
+                percentageBonusCache[bonus.stat][bonus.category] = bonus.percentageBonusPerPoint;
+            }
+        }
         
         public int GetProposedPoints(EnhancementCategory category)
         {
@@ -68,6 +102,38 @@ namespace RPG.Stats
         public int GetAssignablePoints()
         {
             return (int)GetComponent<BaseStats>().GetStat(Stat.TotalEnhancementPoints);
+        }
+
+        public IEnumerable<float> GetAdditiveModifiers(Stat stat)
+        {
+            if (!additiveBonusCache.ContainsKey(stat)) yield break;
+
+            foreach (EnhancementCategory category in additiveBonusCache[stat].Keys)
+            {
+                float bonus = additiveBonusCache[stat][category];
+                yield return bonus * GetPoints(category);
+            }
+        }
+
+        public IEnumerable<float> GetPercentageModifiers(Stat stat)
+        {
+            if (!percentageBonusCache.ContainsKey(stat)) yield break;
+
+            foreach (EnhancementCategory category in percentageBonusCache[stat].Keys)
+            {
+                float bonus = percentageBonusCache[stat][category];
+                yield return bonus * GetPoints(category);
+            }
+        }
+
+        public object CaptureState()
+        {
+            return assignedPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            assignedPoints = new Dictionary<EnhancementCategory, int>((IDictionary<EnhancementCategory, int>)state);
         }
     }
 }
