80531e044f16c6df434788c94f56c476c3aa9b93
Arhan Busam <arhan.busam@gmail.com>
2021-07-20 19:30:25 +1000

    auto commit changes

---
+++ cron/index.py
@@ -0,0 +1,22 @@
+from git import Repo
+from datetime import date
+
+working_tree_dir = "/Users/arhanbusam/Dropbox/Arhan's Documents/Unity/RPG/"
+file = "dailyGitActivity/activityTracker.txt"
+repo = Repo(working_tree_dir)
+
+def alter_file(file):
+    today = date.today()
+    with open(file, "a") as f:
+        f.write(today.strftime("%B %d, %Y") + "\n")
+
+def git_activities(repo):
+    if len(repo.untracked_files):
+        today = date.today() 
+        repo.git.add(A=True)
+        repo.git.commit('-m', today.strftime("%B %d, %Y"))
+        repo.git.push('origin', 'HEAD:refs/for/master') 
+
+def commit_routine():
+    alter_file(file)
+    git_activities(repo)
